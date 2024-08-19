0918dc5f51eac195882eeaa4506a0c036532875f
jinho1593 <jinho2658@gmail.com>
2024-08-19 19:47:40 +0900

    Remove cached files that are now in .gitignore

--- library/src/main/java/egovframework/example/main/controller/MainController.java
+++ library/src/main/java/egovframework/example/main/controller/MainController.java
@@ -6,9 +6,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+   // 생성자
+   public MainController() {
+      System.out.println("MainController 생성자 호출됨");
+   }
 
-	@GetMapping("/main")
-	public String mainPage() {
-		return "main";
-	}
+   @GetMapping("/main")
+   public String mainPage() {
+      System.out.println("접근성공");
+      return "main";
+   }
 }
