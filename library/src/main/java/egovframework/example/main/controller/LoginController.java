de1e76a5ead21c7b6c1d6f989161831e179a6b58
jinho1593 <jinho2658@gmail.com>
2024-09-10 12:46:04 +0900

    feat: [김진호] 로그인 회원가입 페이지 폼

--- library/src/main/java/egovframework/example/main/controller/LoginController.java
+++ library/src/main/java/egovframework/example/main/controller/LoginController.java
@@ -1,47 +1,13 @@
 package egovframework.example.main.controller;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class LoginController {
-	
-	private final JdbcTemplate jdbcTemplate;
-	
-	   @Autowired
-	   public LoginController(JdbcTemplate jdbcTemplate) {
-		  this.jdbcTemplate = jdbcTemplate;
-	      System.out.println("MainController 생성자 호출됨");
-	   }
 
-	   @GetMapping("/test")
+	   @GetMapping("/loginPage")
 	   public String mainPage() {
-	      System.out.println("접근성공");
-	      return "test";
-	   }
-	   
-	   @PostMapping("/checkTest")
-	   public String checkTestData(Model model) {
-		   
-		   System.out.println("디비연동시작");
-		   
-		   String sql = "SELECT number From test";
-		   
-		   try {
-			   Integer number = jdbcTemplate.queryForObject(sql, Integer.class);
-			   model.addAttribute("message", number);
-		   } catch (Exception e) {
-			   model.addAttribute("message", e.getMessage());
-			   e.printStackTrace();
-		   }
-		   
-		   System.out.println("디비연동완료");
-		   
-		   return "test";
-		   
+	      System.out.println("로그인페이지 접근성공");
+	      return "loginPage";
 	   }
 }
--- library/src/main/webapp/WEB-INF/jsp/loginPage.jsp
+++ library/src/main/webapp/WEB-INF/jsp/loginPage.jsp
@@ -5,8 +5,35 @@
 <head>
 <meta charset="UTF-8">
 <title>Login</title>
+    <link rel="stylesheet" href="${pageContext.request.contextPath}/resources/css/loginPage.css">
 </head>
 <body>
+	<div class="header">
+		<div class="header-logo">
+		 	<button onclick="location.href='${pageContext.request.contextPath}/main'">
+                <img src="${pageContext.request.contextPath}/resources/images/librarylog.png" alt="로고" class="logo">
+            </button>
+		</div>
+	</div>
+	
+	<div class="login-form">
+	    <div class="input-group">
+	        <input type="text" placeholder="아이디" name="user_id" maxlength="20">
+	    </div>
+	    <div class="input-group">
+	        <input type="password" placeholder="비밀번호" name="password" maxlength="20">
+	        <input class="submit-login" type="submit" value="로그인"> 
+	    </div>
+	</div>
+	
+	<button>간편회원가입</button>
+	
+	<div class="sns-login-form">
+	    <div>SNS 계정으로 로그인</div>
+	    <button>네이버</button>
+	    <button>카카오</button>
+	    <button>구글</button>
+	</div>
 
 </body>
 </html>
